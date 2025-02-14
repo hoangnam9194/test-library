fe80ab9fffe50bac44f916dffed402875d51888f
hoangnam9194 <hoangnam9194@gmail.com>
2025-02-14 15:49:11 +0700

    update gradle

--- library/link-attribution/build.gradle.kts
+++ library/link-attribution/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     id("kotlin-parcelize")
     id("maven-publish")
-    id("signing")
-    id("org.jetbrains.dokka") version "1.9.10"
+//    id("signing")
+//    id("org.jetbrains.dokka") version "1.9.10"
 //    alias(libs.plugins.kotlin.compose)
 }
 
@@ -37,26 +37,26 @@ android {
     }
 
     // Sources JAR
-    task<Jar>("sourcesJar") {
-        archiveClassifier.set("sources")
-        from(android.sourceSets["main"].java.srcDirs)
-    }
+//    task<Jar>("sourcesJar") {
+//        archiveClassifier.set("sources")
+//        from(android.sourceSets["main"].java.srcDirs)
+//    }
+//
+//    task<Jar>("javadocJar") {
+//        archiveClassifier.set("javadoc")
+//        from(tasks["dokkaHtml"]) // Use Dokka for Kotlin documentation
+//    }
 
-    task<Jar>("javadocJar") {
-        archiveClassifier.set("javadoc")
-        from(tasks["dokkaHtml"]) // Use Dokka for Kotlin documentation
-    }
-
-    publishing {
-        // Enable publishing for the release variant
+//    publishing {
+//        // Enable publishing for the release variant
+////        singleVariant("release") {
+////            buildTypeProjectionWithVariance { configurations.getByName("release") }
+////        }
 //        singleVariant("release") {
-//            buildTypeProjectionWithVariance { configurations.getByName("release") }
+//            withSourcesJar()
+//            withJavadocJar()
 //        }
-        singleVariant("release") {
-            withSourcesJar()
-            withJavadocJar()
-        }
-    }
+//    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -102,68 +102,18 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
 }
 
-val libVersion = "1.0.0"
-val libGroup = "dev.hoangnam9194.library"
-val libArtifactId = "linkattributionn"
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.github.hoangnam9194" // Replace with your GitHub username
+                artifactId = "test-library" // Replace with your library's name (e.g., my-awesome-library)
+                version = "1.0.0" // Initial version number (important!)
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = libGroup // e.g., com.example
-            artifactId = libArtifactId // e.g., mylibrary
-            version = libVersion // Your library version
-
-            artifact(tasks["sourcesJar"])
-            artifact(tasks["javadocJar"])
-
-            afterEvaluate {
-                from(components["release"])
-            }
-
-            pom {
-                name.set("My Library Name")
-                description.set("A brief description of your library")
-                url.set("https://github.com/hoangnam9194/test-library")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("hoangnam9194")
-                        name.set("Nam Nguyen")
-                        email.set("hoangnam9194@gmail.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:github.com/hoangnam9194/test-library.git")
-                    developerConnection.set("scm:git:ssh://github.com/hoangnam9194/test-library.git")
-                    url.set("https://github.com/hoangnam9194/test-library/tree/main")
+                afterEvaluate {
+                    from(components["release"])
                 }
             }
         }
     }
-
-    repositories {
-        maven {
-            name = "MavenCentral"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = project.findProperty("ossrhUsername") as String? ?: ""
-                password = project.findProperty("ossrhPassword") as String? ?: ""
-            }
-        }
-    }
-}
-
-signing {
-    val signingKey = System.getenv("GPG_SIGNING_KEY")
-    val signingPassword = System.getenv("GPG_SIGNING_PASSWORD")
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications["release"])
 }
